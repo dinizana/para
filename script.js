9c945c1a188bca6024c6e18eec19f61bf8c8f8fd
dinizana <127898957+dinizana@users.noreply.github.com>
2026-02-25 14:42:11 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function abrirCarta() {
-    document.getElementById('inicio').classList.add('hidden');
-    document.getElementById('carta').classList.remove('hidden');
-}
-
-function fecharCarta() {
-    document.getElementById('carta').classList.add('hidden');
-    document.getElementById('inicio').classList.remove('hidden');
-}
